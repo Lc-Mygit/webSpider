c446c8a2499c1344fedb73aa15f02f11d15c1cd9
licheng <274454439@qq.com>
2019-08-26 00:03:51 +0800

    目标网址:  http://www.xinwentoutiao.net/   爬取  新闻头条-今天头条的热点新闻数据。

--- runSpider/run.js
+++ runSpider/run.js
@@ -1,6 +1,6 @@
 /**
  * @author 李程
- * @description 专门爬取 https://www.vipyl.com/  雨露文章网的数据
+ * @description 专门运行爬虫抓取网站数据的的文件
  * @date  2019-08-24
  * 
  * 
@@ -38,11 +38,11 @@ const runItme = function () {
                 
                 $(".tonglei #artadd").find("li").each(function(index,item){
                     getDataArr.push({   
-                      title:$(this).find(".rightnr a").text(),
-                      description: $(this).find(".rightnr span").text().substring(0,$(this).find(".rightnr span").text().length-10) ,
-                      ImgUrl:WebUrl+$(this).find(".leftimg img").attr("src"),
-                      jumpUrl:WebUrl+$(this).find(".rightnr a").attr("href"),
-                      Date:$(this).find(".rightnr span i").text(),
+                         title:$(this).find(".rightnr a").text(),
+                   //   description: $(this).find(".rightnr span").text().substring(0,$(this).find(".rightnr span").text().length-10) ,
+                   //      ImgUrl:WebUrl+$(this).find(".leftimg img").attr("src"),
+                  //    jumpUrl:WebUrl+$(this).find(".rightnr a").attr("href"),
+                     // Date:$(this).find(".rightnr span i").text(),
                         
                     })
                 });
@@ -52,7 +52,7 @@ const runItme = function () {
             }
         })    
         
-        /*superagent.get(WebUrl).end( (err,res,body)=>{
+        /*superagent.get(WebUrl).end( (err,res)=>{
             if(err){
                 console.log("爬取请求报错了...");
                 return
@@ -74,11 +74,70 @@ const runItme = function () {
     });
 }
 
+/**
+ * @param        目标网址:  http://www.xinwentoutiao.net/ 
+ * @description  爬取  新闻头条-今天头条的热点新闻数据。
+ * @date         2019-08-25 星期日
+ * 
+ */
+ const GetHeadlinesData = function(){
+    return new Promise((resolve,reject)=>{
+        let WebUrl = "http://www.xinwentoutiao.net/";
+        superagent.get("http://www.xinwentoutiao.net/").end( (err,res)=>{
+            if(err){
+                console.log("新闻头条 报错了");
+                return
+            }
+            //运行获取数据的区域
+            let $ = cheerio.load( res.text);
+            let getDataArr = [];//获取到的 存储数据
+            let objData ={};
+            $(".slider .d01").each( function(index,item){
+               if( $(this).find(".title span").text() === "今天头条" ){
+                    $(this).find("ul li").each( function(index,item){
+                        if(  index !== 10 ){ 
+                            $(item).find("table td").each( function(index,item){
+                                    switch(index){
+                                        case 0:
+                                            objData.order = $(item).text();
+                                            break;
+                                        case 1:
+                                            objData.title = $(item).text();
+                                            break;    
+                                        case 2:
+                                            objData.lastTime = $(item).text();
+                                            break;   
+                                        case 3:
+                                            objData.heatCout = $(item).text();
+                                            break;     
+                                    }
+                            });
+                            let tempObj = JSON.stringify( objData );
+                            getDataArr.push( JSON.parse( tempObj ) ); 
+                            objData ={};
+                        }   
+                    });
+               }
+              
+            })
+           
+            resolve(getDataArr)
+
+        });
+
+
+    });
+
+
+
+ }   
+
+
 
 
 
 //把模块推出暴露，让逻辑页面引用。
 module.exports = {
-    runSpider: runItme
-
+   // runSpider: runItme
+   runSpider:GetHeadlinesData
 }
